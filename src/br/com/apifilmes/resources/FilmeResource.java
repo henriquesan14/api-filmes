fca29f7581805069ac1d826870a5789508b4be27
Henrique <ricogx@hotmail.com>
2019-11-10 16:05:04 -0300

    criado classe de erro e tratamentos de 404 e 400

--- src/br/com/apifilmes/resources/FilmeResource.java
+++ src/br/com/apifilmes/resources/FilmeResource.java
@@ -1,5 +1,6 @@
 package br.com.apifilmes.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -11,14 +12,21 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import br.com.apifilmes.dao.FilmeDao;
 import br.com.apifilmes.dao.FilmeDaoImpl;
+import br.com.apifilmes.models.ErrorMessage;
 import br.com.apifilmes.models.Filme;
 
 @Path("/filmes")
 public class FilmeResource {
+	
+	@Context
+	private UriInfo uriInfo;
 	
 	private FilmeDao filmeDao;
 	
@@ -36,30 +44,47 @@ public class FilmeResource {
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Filme save(Filme filme) {
-		return filmeDao.save(filme);
+	public Response save(Filme filme) {
+		filme.setId(null);
+		Filme filmeCadastrado = filmeDao.save(filme);
+		URI uri = uriInfo.getAbsolutePathBuilder().path(filmeCadastrado.getId().toString()).build();
+		return Response.created(uri).build();
 	}
 	
 	
 	@GET
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Filme getById(@PathParam("id") Long id) {
-		return filmeDao.getById(id);
+	public Response getById(@PathParam("id") Long id) {
+		Filme filme = filmeDao.getById(id);
+		if(filme == null) {
+			return Response.status(404).entity(new ErrorMessage("Não foi encontrado filme com id: " + id)).build();
+		}
+		return Response.ok(filme).build();
 	}
 	
 	@PUT
 	@Path("/{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void update(@PathParam("id") Long id, Filme filme) {
+	public Response update(@PathParam("id") Long id, Filme filme) {
+		if(filme.getId() != null && filme.getId() != id) {
+			return Response.status(400).entity(new ErrorMessage("Requisição inválida, id's não conferem")).build();
+		}
+		if(filmeDao.getById(id) == null) {
+			return Response.status(404).entity(new ErrorMessage("Não foi encontrado filme com id: " + id)).build();
+		}
 		filme.setId(id);
 		filmeDao.save(filme);
+		return Response.ok().build();
 	}
 	
 	@DELETE
 	@Path("/{id}")
-	public void remove(@PathParam("id") Long id) {
-		filmeDao.remove(id);
+	public Response remove(@PathParam("id") Long id) {
+		if(filmeDao.getById(id) == null) {
+			return Response.status(404).entity(new ErrorMessage("Não foi encontrado filme com id: " + id)).build();
+		}
+		return Response.ok().build();
 	}
 
 }
